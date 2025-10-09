92dccb49e49a3401bb0c0965b51a6808f13fb536
jonatasferreiraa <jonferreira.psd@gmail.com>
2025-10-09 10:20:17 -0300

    Funcionalidade: Lógica para adicionar novo item na lista ao clicar no botão

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const addItem = document.getElementById("new-item");
+const addItem = document.getElementById("items");
 const form = document.querySelector("form");
-const newItem = document.querySelector("li");
+const items = document.querySelector("ul");
 
 // Manipulação de input para não receber números.
 
@@ -12,4 +12,34 @@ addItem.addEventListener("input", () => {
 
 form.onsubmit = (event) => {
   event.preventDefault();
+
+  // Garantindo que itens e espaços vazios não sejam adicionados a lista.
+
+  const value = addItem.value.trim();
+  if (value === "") {
+    return;
+  }
+
+  //Criando elemento e aplicando na lista.
+
+  const newItem = document.createElement("li");
+  const nameItem = document.createElement("span");
+  const checkbox = document.createElement("input");
+  const trashIcon = document.createElement("img");
+
+  checkbox.type = "checkbox";
+  checkbox.name = "checkbox-items";
+  checkbox.id = "check-items";
+
+  newItem.className = "products";
+  nameItem.innerText = value;
+
+  trashIcon.src = "/icons/trash.svg";
+  trashIcon.alt = "ícone de Lixeira";
+  trashIcon.className = "trashed";
+
+  newItem.append(checkbox, nameItem, trashIcon);
+  items.prepend(newItem);
+
+  addItem.value = "";
 };
